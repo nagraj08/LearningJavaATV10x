9e0bf21c08cb08f9845206fee00513e73241b7a8
Exercise <nagraj.bitla08@gmail.com>
2025-01-11 11:19:22 +0530

    Using for Loop

--- src/Java_Exercises_02/Variables.java
+++ src/Java_Exercises_02/Variables.java
@@ -3,26 +3,11 @@ package Java_Exercises_02;
 public class Variables {
     public static void main(String[] args) {
         int n = 9;
-        System.out.println();
-        System.out.printf("9 x 1 = %d",n*1);
-        System.out.println();
-        System.out.printf("9 x 2 = %d",n*2);
-        System.out.println();
-        System.out.printf("9 x 3 = %d",n*3);
-        System.out.println();
-        System.out.printf("9 x 4 = %d",n*4);
-        System.out.println();
-        System.out.printf("9 x 5 = %d",n*5);
-        System.out.println();
-        System.out.printf("9 x 6 = %d",n*6);
-        System.out.println();
-        System.out.printf("9 x 7 = %d",n*7);
-        System.out.println();
-        System.out.printf("9 x 8 = %d",n*8);
-        System.out.println();
-        System.out.printf("9 x 9 = %d",n*9);
-        System.out.println();
-        System.out.printf("9 x 10 = %d",n*10);
+        for (int i = 1; i <= 10; i++)
+        {
+            System.out.printf("%d x %d = %d",n,i,n * i);
+            System.out.println();
+        }
 
     }
 }
