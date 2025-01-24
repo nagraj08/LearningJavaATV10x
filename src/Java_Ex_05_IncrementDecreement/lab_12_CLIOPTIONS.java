55e12965a8008c1009b7b1768f44770ef9bbaeba
Exercise <nagraj.bitla08@gmail.com>
2025-01-24 19:21:19 +0530

    CLI Options

---
+++ src/Java_Ex_05_IncrementDecreement/lab_12_CLIOPTIONS.java
@@ -0,0 +1,8 @@
+package Java_Ex_05_IncrementDecreement;
+
+public class lab_12_CLIOPTIONS {
+    public static void main(String[] args) {
+        //Take a user input - Name, Age and Salary and print them in the end.
+        System.out.println("Name="+args[0]+";Age= "+args[1]+"; Salary= "+args[2]);
+    }
+}
