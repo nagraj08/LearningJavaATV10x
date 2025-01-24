77ab86cb5deb520abf08677865f7107c5966552d
Exercise <nagraj.bitla08@gmail.com>
2025-01-24 19:14:19 +0530

    New Code

---
+++ src/Java_Ex_05_IncrementDecreement/lab_10_POSTIncreement.java
@@ -0,0 +1,7 @@
+package Java_Ex_05_IncrementDecreement;
+
+public class lab_10_POSTIncreement {
+    public static void main(String[] args) {
+        int a = 10; System.out.println(++a + a++ + a++); System.out.println(a);
+    }
+}
