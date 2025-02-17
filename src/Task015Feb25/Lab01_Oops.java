9f7b4801da76ffe81762db52106db00fa501ec6e
Exercise <nagraj.bitla08@gmail.com>
2025-02-17 18:18:34 +0530

    09 Feb 25 Array Task

---
+++ src/Task015Feb25/Lab01_Oops.java
@@ -0,0 +1,19 @@
+package Task015Feb25;
+
+public class Lab01_Oops {
+    public static void main(String[] args) {
+        Student newstudent = new Student();
+        System.out.println(newstudent.name="Nagraj");
+        System.out.println(newstudent.rollno=18);
+        System.out.println(newstudent.age=47);
+    }
+}
+    class Student
+    {
+        String name;
+        int rollno;
+        int age;
+
+        // void sleep();
+    }
+
