ec007504a9f6bec5826917f062cb703eacf943f8
Exercise <nagraj.bitla08@gmail.com>
2025-02-17 18:21:16 +0530

    15 Feb 25 Oops Labs

---
+++ src/Task015Feb25/lab_02_Oops.java
@@ -0,0 +1,16 @@
+package Task015Feb25;
+
+public class lab_02_Oops {
+    //attributes
+    String name;
+    int age;
+    String gender;
+    int Salary;
+    long phone_no;
+    String address;
+    //Methods
+    void sleep();
+    void speak();
+    void eat();
+    void walk();
+}
