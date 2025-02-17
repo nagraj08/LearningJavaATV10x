699395158148fd60e58d2ffea5a00ddf91e80e51
Exercise <nagraj.bitla08@gmail.com>
2025-02-17 18:27:15 +0530

    15 Feb 25 Oops Labs

--- src/Task015Feb25/Lab01_Oops.java
+++ src/Task015Feb25/Lab01_Oops.java
@@ -3,6 +3,9 @@ package Task015Feb25;
 public class Lab01_Oops {
     public static void main(String[] args) {
         Student newstudent = new Student();
+        //Student - Class loading
+        // newstudent - Object reference
+        //new Student() - Class creation
         System.out.println(newstudent.name="Nagraj");
         System.out.println(newstudent.rollno=18);
         System.out.println(newstudent.age=47);
