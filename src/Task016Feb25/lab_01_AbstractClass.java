9930f8e4172573fe212c9501c204a0780c4d5ff2
Exercise <nagraj.bitla08@gmail.com>
2025-02-19 16:16:30 +0530

    18 Feb 25 Oops Labs-Abstract Class task

--- src/Task016Feb25/lab_01_AbstractClass.java
+++ src/Task016Feb25/lab_01_AbstractClass.java
@@ -1,5 +1,21 @@
 package Task016Feb25;
 
 public class lab_01_AbstractClass {
+    public static void main(String[] args) {
+    PrintMyBook myBook = new PrintMyBook();
+    myBook.getDetails("Harry Potter", "J.K.Aowling", "120");
+    }
+}
+abstract class book {
+    public abstract void getDetails(String Name,String Author, String Price);
+}
 
+class PrintMyBook extends book{
+
+
+    @Override
+    public void getDetails(String Name, String Author, String Price) {
+        System.out.println(Name + Author + Price);
+
+    }
 }
